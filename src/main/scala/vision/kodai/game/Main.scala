b7b5f20ac3588df8dc3fc0ba2e149eb5398147ab
0918nobita <nobita.0918@gmail.com>
2020-07-03 01:55:35 +0900

    :construction: Scala プログラム内で LWJGL をインポートするようにした

--- src/main/scala/vision/kodai/game/Main.scala
+++ src/main/scala/vision/kodai/game/Main.scala
@@ -1,7 +1,19 @@
 package vision.kodai.game
 
+import org.lwjgl.glfw.GLFW.glfwInit
+
+class Game {
+  def init(): Unit = {
+    if (!glfwInit())
+      throw new IllegalStateException("Unable to initialize GLFW")
+  }
+}
+
 object Main {
   def main(args: Array[String]): Unit = {
+    System.setProperty("org.lwjgl.util.Debug", "true")
+    // TODO: UnsatisfiedLinkError が発生しないように修正する
+    // new Game().init()
     println("Hello, world!")
   }
 }
