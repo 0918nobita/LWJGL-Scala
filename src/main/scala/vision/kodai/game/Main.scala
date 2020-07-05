0c1f2ef57c085f110835f0b3c8ff718d01e67f55
0918nobita <nobita.0918@gmail.com>
2020-07-06 01:28:11 +0900

    :sparkles: 型安全な Builder に関するサンプルを追加

--- src/main/scala/vision/kodai/game/Main.scala
+++ src/main/scala/vision/kodai/game/Main.scala
@@ -1,7 +1,13 @@
 package vision.kodai.game
 
+import glfw.Builder
+
 object Main {
   def main(args: Array[String]): Unit = {
+    val window =
+      Builder().withWidth(600).withHeight(400).withTitle("Game").build
+    println(window) // => Window (600, 400, `Game`)
+
     System.setProperty("org.lwjgl.system.allocator", "system")
     System.setProperty("org.lwjgl.util.Debug", "true")
     new Game().run()
--- src/main/scala/vision/kodai/game/glfw/WindowBuilder.scala
+++ src/main/scala/vision/kodai/game/glfw/WindowBuilder.scala
@@ -7,6 +7,37 @@ import org.lwjgl.glfw.GLFW.{
 }
 import org.lwjgl.system.MemoryUtil.NULL
 
+sealed trait Property
+object Property {
+  sealed trait Empty  extends Property
+  sealed trait Width  extends Property
+  sealed trait Height extends Property
+  sealed trait Title  extends Property
+  type Mandatory = Empty with Width with Height with Title
+}
+
+case class Builder[P <: Property] private (
+    width: Int = 0,
+    height: Int = 0,
+    title: String = ""
+) {
+  def withWidth(w: Int): Builder[P with Property.Width] =
+    this.copy(width = w)
+
+  def withHeight(h: Int): Builder[P with Property.Height] =
+    this.copy(height = h)
+
+  def withTitle(t: String): Builder[P with Property.Title] =
+    this.copy(title = t)
+
+  def build(implicit ev: P =:= Property.Mandatory) =
+    s"Window ($width, $height, `$title`)"
+}
+
+object Builder {
+  def apply(): Builder[Property.Empty] = new Builder()
+}
+
 class WindowBuilder {
 
   /** デフォルトの各種 Hint (特性) の読み込み */
